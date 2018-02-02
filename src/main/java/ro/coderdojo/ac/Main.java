4943df0c20ffc3a97b5b54396e3cbb512441240a
Andrei Vasilache <andrei.vasilache.ro@gmail.com>
2018-02-02 20:12:55 +0200

    crearea proiect

---
+++ src/main/java/ro/coderdojo/ac/Main.java
@@ -0,0 +1,18 @@
+package ro.coderdojo.ac;
+
+import java.util.logging.Level;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+import ro.coderdojo.ac.CoderDojoCommand;
+import ro.coderdojo.ac.EventsListener;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+		getServer().getPluginManager().registerEvents(new EventsListener(), this);
+		
+		this.getCommand("CoderDojo").setExecutor(new CoderDojoCommand());
+    }
+
+}
