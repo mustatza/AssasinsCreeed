fe18874366f945af0135b621bdee84f63b9db2a2
Andrei Vasilache <andrei.vasilache.ro@gmail.com>
2018-02-07 21:35:55 +0200

    ccg

--- src/main/java/ro/coderdojo/ac/EventsListener.java
+++ src/main/java/ro/coderdojo/ac/EventsListener.java
@@ -1,13 +1,19 @@
+
 package ro.coderdojo.ac;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,9 +21,14 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import ro.coderdojo.ac.CoderDojoCommand;
+
+
 
 public final class EventsListener implements Listener {
 
@@ -82,6 +93,14 @@ public final class EventsListener implements Listener {
         }
         event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 18000, 2));
         event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 1));
+    }
+
+    @EventHandler
+    public void playerJoined(PlayerJoinEvent event) throws Exception {
+        Player player = event.getPlayer();
+        player.setGameMode(GameMode.SURVIVAL);
+        player.teleport(new Location(event.getPlayer().getWorld(), -1598.383, 64.00000, -220.431, -89.8f, 6.9f));
+
     }
 
 }
